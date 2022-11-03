ef9a6912650504a9008d53d1807d4920faa48d0f
Ari Glockner <44281741+AriGlockner@users.noreply.github.com>
2022-11-03 14:05:33 -0400

    Cleaned up code

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ import matplotlib.pyplot as plt
 # Get Random x and y values
 minValue = 0
 maxValue = 1000
-size = 50
-x = np.random.random_integers(minValue, maxValue, size)
-y = np.random.random_integers(minValue, maxValue, size)
+size = 100
+
+x = np.random.randint(minValue, maxValue + 1, size)
+y = np.random.randint(minValue, maxValue + 1, size)
 
 # plot x and y
-plt.plot(x, y, linestyle='none', marker='o', markerfacecolor='blue', markersize=5)
+plt.plot(x, y, linestyle='none', marker='o', markerfacecolor='blue', markersize=3)
 
 # Make Graph Fancier
 plt.xlabel("x")
 plt.ylabel("y")
+plt.title("Plot")
 
+
+# Show the graph
 plt.show()
