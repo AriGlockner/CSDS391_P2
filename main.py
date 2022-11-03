adc2ac5220983d2d432ba64d102390224e6189b4
Ari Glockner <44281741+AriGlockner@users.noreply.github.com>
2022-11-03 13:53:48 -0400

    Created Plot

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("Hello, World!")
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Get Random x and y values
+minValue = 0
+maxValue = 1000
+size = 50
+x = np.random.random_integers(minValue, maxValue, size)
+y = np.random.random_integers(minValue, maxValue, size)
+
+# plot x and y
+plt.plot(x, y, linestyle='none', marker='o', markerfacecolor='blue', markersize=5)
+
+# Make Graph Fancier
+plt.xlabel("x")
+plt.ylabel("y")
+
+plt.show()
