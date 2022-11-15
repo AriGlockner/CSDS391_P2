6808a421b87a645828766e780b5ecdf6d6172bca
Ari Glockner <44281741+AriGlockner@users.noreply.github.com>
2022-11-15 16:51:43 -0500

    Finished part of P1 Clustering

--- main.py
+++ main.py
@@ -2,26 +2,57 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
-with open('CSDS391_P2\irisdata.csv', mode = 'r') as file:
-    iris = csv.reader(file)
-    x = 0
+with open('CSDS391_P2\irisdata.csv') as file:
+    # Used to take out the header from the file
+    heading = next(file)
 
-    for lines in iris:
+    # iris data
+    iris = csv.reader(file)
+
+    # Deriving a learning rule -> uk = Sum of n (r * x) / Sum of n (r)
+    ukx = [0.0, 0.0, 0.0]
+    uky = [0.0, 0.0, 0.0]
+    rnk = [0.0, 0.0, 0.0]
+
+    # Getting
+    D = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+    # Plot the species
+    for row in iris:
+        # Get the color based on the species
         color = 'black'
-        if lines[4] == 'setosa':
+        if row[4] == 'setosa':
             color = 'blue'
-        elif lines[4] == 'versicolor':
+            ukx[0] += float(row[1])
+            uky[0] += float(row[2])
+            rnk[0] += 1
+        elif row[4] == 'versicolor':
             color = 'red'
-        elif lines[4] == 'virginica':
+            ukx[1] += float(row[1])
+            uky[1] += float(row[2])
+            rnk[1] += 1
+        elif row[4] == 'virginica':
             color = 'green'
+            ukx[2] += float(row[1])
+            uky[2] += float(row[2])
+            rnk[2] += 1
         else:
             color = 'black'
+        # plot the species
+        plt.plot(float(row[1]), float(row[2]), linestyle='none', marker='o', markerfacecolor=color)
 
-        plt.plot(lines, linestyle='none', marker='o', markerfacecolor=color)
+    for i in range(3):
+        ukx[i] /= rnk[i]
+        uky[i] /= rnk[i]
 
+    plt.plot(ukx, uky, linestyle='none', marker='o', markerfacecolor='black')
 
+    # Objective function
+    
+    # petal_length,petal_width
+    plt.xlabel('Petal Length')
+    plt.ylabel('Petal Width')
     plt.show()
-
 
 
 '''
